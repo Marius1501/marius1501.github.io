a59ed9ad219edbe513d8d1b55431f08c72556bba
ge78fug <ge78fug@mytum.de>
2023-05-09 15:41:37 +0200

    updated courses.js

--- assets/js/courses.js
+++ assets/js/courses.js
@@ -1,32 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let http = new XMLHttpRequest();
+let courses = [];
 
-    http.open('get', 'assets/json/courses.json', true);
-    http.send();
-    http.onload = function () {
-        if (this.readyState === 4 && this.status === 200) {
-            let courses = JSON.parse(this.responseText);
-            let output = "";
+fetch('assets/json/courses.json')
+    .then(response => response.json())
+    .then(data => {
+        courses = data;
+        let output = "";
 
-            for (let item of courses) {
-                output += `
-                  <div class="box">
-                      <div class="box-image">
-                          <a href="${item.link}" title="${item.title}" target="_blank"><img src="${item.image}"></a>
-                      </div>
-                      <div class="box-content">
-                          <h3 class="box-caption">${item.title}</h3>
-                          <p class="box-text">${item.text}</p>
-                      </div>
+        for (let item of courses) {
+            output += `
+                <div class="box">
+                  <div class="box-image">
+                      <a href="${item.link}" title="${item.title}" target="_blank"><img src="${item.image}" alt="${item.title}"></a>
                   </div>
-              `;
-            }
-            document.querySelector(".box-container").innerHTML = output;
-
-            const boxes = document.querySelectorAll(".box");
-            courses.forEach((course, index) => {
-                course.element = boxes[index];
-            });
+                  <div class="box-content">
+                      <h3 class="box-caption">${item.title}</h3>
+                      <p class="box-text">${item.text}</p>
+                  </div>
+                </div>
+            `;
         }
-    }
-});
+        document.querySelector(".box-container").innerHTML = output;
+
+        const boxes = document.querySelectorAll(".box");
+        courses.forEach((course, index) => {
+            course.element = boxes[index];
+        });
+    })
+    .catch(error => console.log(error));
